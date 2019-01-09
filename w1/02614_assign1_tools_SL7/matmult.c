9393a5103392c80a8d71ea9ffe8ab3ac5c030a64
Alexandros Dorodoulis <aldor94@gmail.com>
2019-01-09 13:53:23 +0100

    feat: Add fix makefile

--- w1/02614_assign1_tools_SL7/matmult.c
+++ w1/02614_assign1_tools_SL7/matmult.c
@@ -1,6 +1,8 @@
 #include "matmult.h"
+#include "stdio.h"
 
-void matmult_nat(int m, int n, int k, double **A, double **B, double **C){
+void matmult_nat(int m, int n, int k, double **A, double **B, double **C)
+{
     // m : number of rows of A
     // k : number of cols of A and number of rows of B
     // n : number of cols of B
@@ -9,31 +11,38 @@ void matmult_nat(int m, int n, int k, double **A, double **B, double **C){
     int i, j, p;
 
     // feeding matrix A
-    for(i=0; i < m; i++){
-        for(j=0; j < k; j++){
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < k; j++)
+        {
             A[i][j] = 2.0;
         }
     }
 
     // feeding matrix B
-    for(i=0; i < k; i++){
-        for(j=0; j < n; j++){
+    for (i = 0; i < k; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
             B[i][j] = 2.0;
         }
     }
 
     // computing and printing matrix product
-    for(i=0; i < m; i++){
-        for(j=0;j < n; j++){
-            for(p=0; p < k; p++){
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            for (p = 0; p < k; p++)
+            {
                 C[i][j] += A[i][p] * B[p][j];
             }
-        printf("%.3f  ", C[i][j]);
+            printf("%.3f  ", C[i][j]);
         }
         printf("\n");
     }
 }
 
-void matmult_blk(int m,int n,int k,double **A,double **B, double **C, int bs){
-
+void matmult_blk(int m, int n, int k, double **A, double **B, double **C, int bs)
+{
 }
