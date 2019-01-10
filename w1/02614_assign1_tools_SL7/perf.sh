38e532fdff667af0d39e403061047a10ad8a2f2b
Alexandros Dorodoulis <aldor94@gmail.com>
2019-01-10 14:29:39 +0100

    fix: Drop unused cores

--- w1/02614_assign1_tools_SL7/perf.sh
+++ w1/02614_assign1_tools_SL7/perf.sh
@@ -4,9 +4,9 @@
 #BSUB -e matmult_%J.err
 #BSUB -q hpcintro
 #BSUB -W 2000000 -R "rusage[mem=15012MB]"
-#BSUB -R "span[hosts=1]"
-#BSUB -n 32
+#BSUB -n 6
 
+echo "Starting Jobs"
 timestamp=`date +"%T"`
 
 COMPILER_OPTIONS=(O0 O1 O2 O3 Os Og Ofast)
@@ -19,13 +19,11 @@ for c in "${COMPILER_OPTIONS[@]}"; do
 	COMPILER_OPTIONS=-$c make
 	mkdir ${timestamp}/${c}
 		for f in "${FUNCTIONS[@]}"; do
-			echo $f
 			mkdir ${timestamp}/${c}/${f}
 			for i in `seq 1 80 2000`;do
 				./matmult_c.gcc ${f} $i $i $i >>${timestamp}/${c}/${f}/${i}.txt 2>&1 & WAITPIDS="$WAITPIDS "$!
 			done
 	done
-	echo $WAITPIDS
 	wait $WAITPIDS
-	echo "DONE"
 done
+echo "DONE"
