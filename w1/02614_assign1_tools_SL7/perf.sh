beb53d6e9e385d831ecbdb9c959e32e145ca80d7
Alexandros Dorodoulis <aldor94@gmail.com>
2019-01-10 14:10:47 +0100

    feat: Parallelize bash script

--- w1/02614_assign1_tools_SL7/perf.sh
+++ w1/02614_assign1_tools_SL7/perf.sh
@@ -9,17 +9,21 @@ timestamp=`date +"%T"`
 
 COMPILER_OPTIONS=(O0 O1 O2 O3 Os Og Ofast)
 
-FUNC=(mnk mkn nmk nkm kmn knm)
+FUNCTIONS=(mnk mkn nmk nkm kmn knm)
 
 mkdir "${timestamp}"
-for f in "${FUNC}"; do
-	mkdir ${timestamp}/${f}
-	for c in "${COMPILER_OPTIONS[@]}"; do
-		mkdir ${timestamp}/${f}/${c}
-		for i in `seq 1 80 2000`;do
-			make clean
-			COMPILER_OPTIONS=-$c make
-			./matmult_c.gcc ${f} $i $i $i >>${timestamp}/${f}/${c}/${i}.txt 2>&1
-		done
+for c in "${COMPILER_OPTIONS[@]}"; do
+	mkdir ${timestamp}/${c}
+		for f in "${FUNCTIONS[@]}"; do
+			echo $f
+			mkdir ${timestamp}/${c}/${f}
+			for i in `seq 1 80 2000`;do
+				make clean
+				COMPILER_OPTIONS=-$c make
+				./matmult_c.gcc ${f} $i $i $i >>${timestamp}/${c}/${f}/${i}.txt 2>&1 & WAITPIDS="$WAITPIDS "$!
+			done
 	done
+	echo $WAITPIDS
+	wait $WAITPIDS
+	echo "DONE"
 done
