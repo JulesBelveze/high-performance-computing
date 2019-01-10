1ad1393a72499ffdb9678cc9d6c5fda9aa9190e0
JulesBelveze <jules.belveze@hotmail.fr>
2019-01-10 12:49:27 +0100

    blk size data

--- w1/02614_assign1_tools_SL7/print_blk.sh
+++ w1/02614_assign1_tools_SL7/print_blk.sh
@@ -1,3 +1,9 @@
+# BSUB -J blk
+# BSUB -o blk_%J.out
+# BSUB -e blk_%J.err
+# BSUB -q hpcintro
+# BSUB -W 200 -R "rusage[mem=5012MB]"
+
 # run matmult_c for different matrix size
 # for i in `seq 100 100 2000`
 # do
@@ -9,10 +15,10 @@
 # done
 
 # data with different block size
-for i in `seq 1 10 151`
+for i in `seq 0 10 300`
 do
+    ./matmult_c.gcc blk 1250 1250 1250 $i
     ./matmult_c.gcc blk 1500 1500 1500 $i
     ./matmult_c.gcc blk 1750 1750 1750 $i
-    ./matmult_c.gcc blk 2000 2000 2000 $i
     echo " "
 done
