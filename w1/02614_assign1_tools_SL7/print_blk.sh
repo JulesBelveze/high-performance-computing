6f77012eaed5ba3c14c7b4e52d4c7482a053fff8
JulesBelveze <jules.belveze@hotmail.fr>
2019-01-10 10:31:13 +0100

    rename files + bash script

--- w1/02614_assign1_tools_SL7/print_blk.sh
+++ w1/02614_assign1_tools_SL7/print_blk.sh
@@ -1,5 +1,17 @@
-for i in `seq 100 20 2000`
-do
-    ./matmult_c.gcc knm $i $i $i
-done
+# run matmult_c for different matrix size
+# for i in `seq 100 100 2000`
+# do
+#     start=`date +%s%3N`
+#     ./matmult_c.gcc knm $i $i $i
+#     end=`date +%s%3N`
+#     runtime=$((end-start))
+#     echo $runtime
+# done
 
+for i in `seq 1 10 151`
+do
+    ./matmult_c.gcc blk 1500 1500 1500 $i
+    ./matmult_c.gcc blk 1750 1750 1750 $i
+    ./matmult_c.gcc blk 2000 2000 2000 $i
+    echo " "
+done
