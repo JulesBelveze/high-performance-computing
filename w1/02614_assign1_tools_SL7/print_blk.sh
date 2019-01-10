d3211162e3112bedf99ffe1df8a85ed6111e2ada
JulesBelveze <jules.belveze@hotmail.fr>
2019-01-10 13:57:43 +0100

    bash script

--- w1/02614_assign1_tools_SL7/print_blk.sh
+++ w1/02614_assign1_tools_SL7/print_blk.sh
@@ -2,23 +2,72 @@
 # BSUB -o blk_%J.out
 # BSUB -e blk_%J.err
 # BSUB -q hpcintro
-# BSUB -W 200 -R "rusage[mem=5012MB]"
+# BSUB -W 2000000000 -R "rusage[mem=5012MB]"
+
+make clean
+make
 
 # run matmult_c for different matrix size
 # for i in `seq 100 100 2000`
 # do
-#     start=`date +%s%3N`
-#     ./matmult_c.gcc knm $i $i $i
-#     end=`date +%s%3N`
-#     runtime=$((end-start))
-#     echo $runtime
+#     echo "knm"
+#     for j in `seq 1 1 5`
+#     do
+#         ./matmult_c.gcc knm $i $i $i
+#     done
+#
+#     echo "kmn"
+#     for j in `seq 1 1 5`
+#     do
+#         ./matmult_c.gcc kmn $i $i $i
+#     done
+#
+#     echo "mnk"
+#     for j in `seq 1 1 5`
+#     do
+#         ./matmult_c.gcc mnk $i $i $i
+#     done
+#
+#     echo "mkn"
+#     for j in `seq 1 1 5`
+#     do
+#         ./matmult_c.gcc mkn $i $i $i
+#     done
+#
+#     echo "nkm"
+#     for j in `seq 1 1 5`
+#     do
+#         ./matmult_c.gcc nkm $i $i $i
+#     done
+#
+#     echo "nmk"
+#     for j in `seq 1 1 5`
+#     do
+#         ./matmult_c.gcc nmk $i $i $i
+#     done
+#
+#     echo "\n $i"
 # done
 
 # data with different block size
-for i in `seq 0 10 300`
+for i in `seq 0 10 350`
 do
-    ./matmult_c.gcc blk 1250 1250 1250 $i
-    ./matmult_c.gcc blk 1500 1500 1500 $i
-    ./matmult_c.gcc blk 1750 1750 1750 $i
-    echo " "
+    echo "mat1"
+    for j in `seq 1 1 5`
+    do
+        ./matmult_c.gcc blk 1250 1250 1250 $i
+    done
+
+    echo "mat2"
+    for j in `seq 1 1 5`
+    do
+        ./matmult_c.gcc blk 1500 1500 1500 $i
+    done
+
+    echo "mat3"
+    for j in `seq 1 1 5`
+    do
+        ./matmult_c.gcc blk 1750 1750 1750 $i
+    done
+    echo "\n $i"
 done
