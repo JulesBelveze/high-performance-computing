b12f381d180fb650c4f19c314e1165bab2b59ca3
Alexandros Dorodoulis <aldor94@gmail.com>
2019-01-10 14:11:34 +0100

    feat: Make once per compiler option

--- w1/02614_assign1_tools_SL7/perf.sh
+++ w1/02614_assign1_tools_SL7/perf.sh
@@ -13,13 +13,13 @@ FUNCTIONS=(mnk mkn nmk nkm kmn knm)
 
 mkdir "${timestamp}"
 for c in "${COMPILER_OPTIONS[@]}"; do
+	make clean
+	COMPILER_OPTIONS=-$c make
 	mkdir ${timestamp}/${c}
 		for f in "${FUNCTIONS[@]}"; do
 			echo $f
 			mkdir ${timestamp}/${c}/${f}
 			for i in `seq 1 80 2000`;do
-				make clean
-				COMPILER_OPTIONS=-$c make
 				./matmult_c.gcc ${f} $i $i $i >>${timestamp}/${c}/${f}/${i}.txt 2>&1 & WAITPIDS="$WAITPIDS "$!
 			done
 	done
