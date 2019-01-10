89dc39eab5ccd7ca5976a6baaedbe77ea8c9b542
Alexandros Dorodoulis <aldor94@gmail.com>
2019-01-10 11:16:18 +0100

    feat: Add job params

--- w1/02614_assign1_tools_SL7/perf.sh
+++ w1/02614_assign1_tools_SL7/perf.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#BSUB -J matmult
+#BSUB -o matmult_%J.out
+#BSUB -e matmult_%J.err
+#BSUB -q hpcintro
+#BSUB -W 200 -R "rusage[mem=5012MB]"
 
 timestamp=`date +"%T"`
 
@@ -11,7 +16,7 @@ for f in "${FUNC}"; do
 	mkdir ${timestamp}/${f}
 	for c in "${COMPILER_OPTIONS[@]}"; do
 		mkdir ${timestamp}/${f}/${c}
-		for i in `seq 100 20 2000`;do
+		for i in `seq 1 80 2000`;do
 			make clean
 			COMPILER_OPTIONS=-$c make
 			./matmult_c.gcc ${f} $i $i $i >>${timestamp}/${f}/${c}/${i}.txt 2>&1
