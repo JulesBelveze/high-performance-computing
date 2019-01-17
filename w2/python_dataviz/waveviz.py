d1a357c54ae4e7f5b055d45e319614a37d4feb6f
DannyDannyDanny <s144222@student.dtu.dk>
2019-01-17 14:14:13 +0100

    chore: added new python visualization

--- w2/python_dataviz/waveviz.py
+++ w2/python_dataviz/waveviz.py
@@ -11,7 +11,12 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 #%%
 # b = "17:27:28"
 # b = "10:34:49"
-b = "10:39:16"
+# b = "10:39:16"
+# b = "12:02:39"
+# b = "12:08:50"
+# b = "13:48:53"
+b = "13:59:44"
+
 textfiles = [t for t in glob.glob("**/" + b + "/*.txt", recursive=True)]
 textfiles
 
@@ -23,6 +28,8 @@ for t in textfiles:
     data[N] = readl
 
 # %%
+
+data[4]
 
 # %%
 for n in sorted(list(data.keys())):
@@ -41,43 +48,37 @@ for n in sorted(list(data.keys())):
     # ax.zaxis.set_major_locator(LinearLocator(10))
     # ax.zaxis.set_major_formatter(FormatStrFormatter("%.02f"))
     fig.colorbar(surf)
-    # ax.view_init(230, 30)
+    fdfd = 1
+    plt.xlim([-fdfd, fdfd])
+    plt.ylim([fdfd, -fdfd])
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    # plt.set_zlim([0,20])
+    ax.set_zlabel("Z")
+    ax.set_zlim(0, 30)
+    ax.view_init(30, 240)
     # ax.view_init(230, 60)
     # plt.savefig("./images/runtime_allopts_topops_zoom.png", dpi=900)
     plt.show()
 # %%
+for n in sorted(list(data.keys()))[0:4]:
+    d = data[n]
+    N = int(d[1].split(" ")[1])
+    print(N)
+    ZZ = [x for b in d[4:-1] for x in [list(map(float, b.split(" ")[:-1]))]]
+    Z = np.array(ZZ)
+    X = np.arange(-1, 1, 2 / (N + 2))
+    Y = np.arange(-1, 1, 2 / (N + 2))
+    X, Y = np.meshgrid(X, Y)
 
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-import numpy as np
+    fig, ax = plt.subplots()
 
+    for i in range(Z.shape[0]):
+        for j in range(Z.shape[1]):
+            ax.scatter(i, j, color="b", s=20, alpha=Z[i, j] / 20, edgecolors="none")
+            # print(i,j,Z[i,j],Z[i,j]/20)
 
-fig = plt.figure()
-ax = fig.gca(projection="3d")
-
-# Make data.
-X = np.arange(-5, 5, 0.25)
-Y = np.arange(-5, 5, 0.25)
-X, Y = np.meshgrid(X, Y)
-
-
-R = np.sqrt(X ** 2 + Y ** 2)
-
-Z = np.sin(R)
-
-# Plot the surface.
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-
-# Customize the z axis.
-ax.set_zlim(-1.01, 1.01)
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter("%.02f"))
-
-# Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
-
-plt.show()
+    plt.show()
 
 #%%
+x
