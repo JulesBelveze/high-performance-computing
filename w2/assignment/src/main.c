ac74da8206bca66f8365a00d25a204b8961155ef
DannyDannyDanny <s144222@student.dtu.dk>
2019-01-17 17:20:30 +0100

    fix: variable declaration moved outside for loops

--- w2/assignment/src/main.c
+++ w2/assignment/src/main.c
@@ -13,9 +13,10 @@
 
 void print_matrix(int n, double **Matrix)
 {
-	for (int j = 0; j < n; j++)
+	int i, j;
+	for (j = 0; j < n; j++)
 	{
-		for (int i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
 			printf("%.2lf ", Matrix[i][j]);
 		}
