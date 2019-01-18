071f9b9b2d517660c92941352ab29e68e90229b5
Alexandros Dorodoulis <aldor94@gmail.com>
2019-01-18 16:25:53 +0100

    fix: Improve perf

--- w2/assignment/src/jacobi_parallel_vDanny.c
+++ w2/assignment/src/jacobi_parallel_vDanny.c
@@ -13,6 +13,7 @@ void jacobi_parallel_d(int n, int num_iterations, double **f, double **u, double
   double dist = 100000000000.0;
   double **u_old = malloc_2d(n + 2, n + 2);
 
+#pragma omp parallel for private(i, j)
   for (i = 0; i <= n + 1; i++)
   {
     for (j = 0; j <= n + 1; j++)
@@ -25,38 +26,33 @@ void jacobi_parallel_d(int n, int num_iterations, double **f, double **u, double
          : dist)
 
  */
-   #pragma omp parallel default(none) shared(n, u, u_old, num_iterations, threshold, f, delta_square, temp, c_dist) private(j, i, k) reduction(+ \
-          : dist)
+#pragma omp parallel default(none) shared(n, u, u_old, num_iterations, threshold, f, delta_square, temp, c_dist) private(j, i, k) reduction(+ \
+                                                                                                                                            : dist)
   {
 
     for (k = 0; k < num_iterations && c_dist > threshold; k++)
     {
-      // printf("k:%d\n", k);
-      // #pragma omp barrier
-      #pragma omp single
+// printf("k:%d\n", k);
+// #pragma omp barrier
+#pragma omp single
       dist = 0.0;
-      #pragma omp for schedule(dynamic)
+#pragma omp for private(i, j) //schedule(dynamic)
       for (i = 1; i <= n; i++)
       {
         for (j = 1; j <= n; j++)
         {
           // printf("Line %d\ti:%d\tj:%d\n", __LINE__,i,j);
-          u[i][j] = u_old[i - 1][j];
-          u[i][j] += u_old[i + 1][j];
-          u[i][j] += u_old[i][j - 1];
-          u[i][j] += u_old[i][j + 1];
-          u[i][j] += delta_square * f[i][j];
-          u[i][j] *= 0.25;
+          u[i][j] = (u_old[i - 1][j] + u_old[i + 1][j] + u_old[i][j - 1] + u_old[i][j + 1] + delta_square * f[i][j]) * 0.25;
           dist = dist + (u[i][j] - u_old[i][j]) * (u[i][j] - u_old[i][j]);
         }
       }
-      // printf("---> DONE: Line %d\n", __LINE__);
-      #pragma omp single
+// printf("---> DONE: Line %d\n", __LINE__);
+#pragma omp single
       {
         c_dist = dist;
         temp = u_old;
         u_old = u;
-        u =  temp;
+        u = temp;
       }
     }
   }
