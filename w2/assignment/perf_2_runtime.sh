24f6c235902850b8d0e748299d0b040df01dc9ca
DannyDannyDanny <s144222@student.dtu.dk>
2019-01-18 20:20:08 +0100

    fix: runtime files

--- w2/assignment/perf_2_runtime.sh
+++ w2/assignment/perf_2_runtime.sh
@@ -5,7 +5,7 @@
 #BSUB -n 1
 
 #N=(2 4 8 16 32 64 128 256 512 1024 2048)
-version=(2 3 4 5)
+version=(2 6 7)
 n=1000
 k=10000
 d=0.0001
@@ -16,11 +16,14 @@ make -f Makefile.HPC
 echo "Starting Jobs"
 timestamp=`date +"%T"`
 # mkdir "results/${timestamp}"
-for v in "${version[@]}"; do
-	# touch "results/${timestamp}/${n}.txt"
-	# ./bin/jacob_gauss.gcc-8 0 $n 1000 0.0001 >> "results/${timestamp}/${n}.txt"
-	sleep 0.1
-	echo /bin/time -f "%e" ./bin/jacob_gauss.gcc $v $n $k $d
-	/bin/time -f "%e" ./bin/jacob_gauss.gcc $v $n $k $d
+
+for t in 1 2 4 8; do
+	for v in "${version[@]}"; do
+		# touch "results/${timestamp}/${n}.txt"
+		# ./bin/jacob_gauss.gcc-8 0 $n 1000 0.0001 >> "results/${timestamp}/${n}.txt"
+		sleep 0.1
+		echo OMP_NUM_THREADS=$t /bin/time -f "%e" ./bin/jacob_gauss.gcc $v $n $k $d
+		OMP_NUM_THREADS=$t /bin/time -f "%e" ./bin/jacob_gauss.gcc $v $n $k $d
+	done
 done
 echo "DONE"
--- w2/assignment/perf_XHPC_run.sh
+++ w2/assignment/perf_XHPC_run.sh
@@ -3,14 +3,14 @@ rsync -avz --delete -e ssh ~/Documents/DTU/Semester\ 10/02614\ -\ High\ Performa
 
 # RUN TASKS
 ssh -tt HPCintro << EOT
-export TERM=xterm
-linuxsh
-cd Documents/HPC/w2/assignment/
-bsub < perf_2_runtime.sh
-sleep 0.1
-exit
-exit
+	export TERM=xterm
+	linuxsh
+	cd Documents/HPC/w2/assignment/
+	bsub < perf_2_runtime.sh
+	sleep 0.1
+	exit
+	exit
 EOT
 
 # PULL FROM HPCintro
-# rsync -avz --delete -e ssh HPCintro://zhome/ff/a/97945/Documents/HPC/ ~/Documents/DTU/Semester\ 10/02614\ -\ High\ Performance\ Computing/HPC
+# rsync -avz --delete -e ssh HPCintro://zhome/ff/a/97945/Documents/HPC/ ~/Documents/DTU/Semester\ 10/02614\ -\ High\ Performance\ Computing/HPC 
