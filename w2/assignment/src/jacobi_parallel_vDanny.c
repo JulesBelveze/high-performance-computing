d85fbda51f4848ed0b130f795cb0e821cd1c0398
Alexandros Dorodoulis <aldor94@gmail.com>
2019-01-18 16:48:54 +0100

    feat: Improve perf for dan version

--- w2/assignment/src/jacobi_parallel_vDanny.c
+++ w2/assignment/src/jacobi_parallel_vDanny.c
@@ -7,13 +7,13 @@ void jacobi_parallel_d(int n, int num_iterations, double **f, double **u, double
 {
   threshold *= threshold;
   double delta_square = 2.0 / (n + 1) * 2.0 / (n + 1);
-  int k=0, i, j;
+  int k = 0, i, j;
   double **temp = NULL;
   double c_dist = 100000000000.0;
   double dist = 100000000000.0;
   double **u_old = malloc_2d(n + 2, n + 2);
 
-  #pragma omp parallel for private(i, j)
+#pragma omp parallel for private(i, j)
   for (i = 0; i <= n + 1; i++)
   {
     for (j = 0; j <= n + 1; j++)
@@ -22,26 +22,26 @@ void jacobi_parallel_d(int n, int num_iterations, double **f, double **u, double
     }
   }
 
-   #pragma omp parallel default(none) firstprivate(k) shared(n, u, u_old, num_iterations, threshold, f, delta_square, temp, c_dist) private(j, i) reduction(+ \
-          : dist)
+#pragma omp parallel default(none) firstprivate(k) shared(n, u, u_old, num_iterations, threshold, f, delta_square, temp, c_dist) private(j, i) reduction(+ \
+                                                                                                                                                         : dist)
   {
 
-    while(k < num_iterations && c_dist > threshold)
+    while (k < num_iterations && c_dist > threshold)
     {
-      #pragma omp single
+#pragma omp single
       dist = 0.0;
-      #pragma omp for
+#pragma omp for private(i, j) nowait
       for (i = 1; i <= n; i++)
       {
         for (j = 1; j <= n; j++)
         {
-          u[i][j] = 0.25* (u_old[i - 1][j] + u_old[i + 1][j] + u_old[i][j - 1] + u_old[i][j + 1] + delta_square * f[i][j]);
+          u[i][j] = 0.25 * (u_old[i - 1][j] + u_old[i + 1][j] + u_old[i][j - 1] + u_old[i][j + 1] + delta_square * f[i][j]);
           dist = dist + (u[i][j] - u_old[i][j]) * (u[i][j] - u_old[i][j]);
         }
       }
 
       k += 1;
-      #pragma omp single
+#pragma omp single
       {
         c_dist = dist;
         temp = u_old;
