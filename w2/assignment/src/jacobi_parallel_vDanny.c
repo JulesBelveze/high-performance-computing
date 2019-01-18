93d016dcb94bba396f6428f92028ca9f3bc4a695
JulesBelveze <jules.belveze@hotmail.fr>
2019-01-18 16:39:01 +0100

    vDanny working

--- w2/assignment/src/jacobi_parallel_vDanny.c
+++ w2/assignment/src/jacobi_parallel_vDanny.c
@@ -7,12 +7,13 @@ void jacobi_parallel_d(int n, int num_iterations, double **f, double **u, double
 {
   threshold *= threshold;
   double delta_square = 2.0 / (n + 1) * 2.0 / (n + 1);
-  int k, i, j;
+  int k=0, i, j;
   double **temp = NULL;
   double c_dist = 100000000000.0;
   double dist = 100000000000.0;
   double **u_old = malloc_2d(n + 2, n + 2);
 
+  #pragma omp parallel for private(i, j)
   for (i = 0; i <= n + 1; i++)
   {
     for (j = 0; j <= n + 1; j++)
@@ -20,37 +21,26 @@ void jacobi_parallel_d(int n, int num_iterations, double **f, double **u, double
       u_old[i][j] = u[i][j];
     }
   }
-/*
-  #pragma omp parallel default(none) shared(n, u, u_old, num_iterations, threshold, f, delta_square, temp) private(j, i, k, c_dist) reduction(+ \
-         : dist)
 
- */
-   #pragma omp parallel default(none) shared(n, u, u_old, num_iterations, threshold, f, delta_square, temp, c_dist) private(j, i, k) reduction(+ \
+   #pragma omp parallel default(none) firstprivate(k) shared(n, u, u_old, num_iterations, threshold, f, delta_square, temp, c_dist) private(j, i) reduction(+ \
           : dist)
   {
 
-    for (k = 0; k < num_iterations && c_dist > threshold; k++)
+    while(k < num_iterations && c_dist > threshold)
     {
-      // printf("k:%d\n", k);
-      // #pragma omp barrier
       #pragma omp single
       dist = 0.0;
-      #pragma omp for schedule(dynamic)
+      #pragma omp for
       for (i = 1; i <= n; i++)
       {
         for (j = 1; j <= n; j++)
         {
-          // printf("Line %d\ti:%d\tj:%d\n", __LINE__,i,j);
-          u[i][j] = u_old[i - 1][j];
-          u[i][j] += u_old[i + 1][j];
-          u[i][j] += u_old[i][j - 1];
-          u[i][j] += u_old[i][j + 1];
-          u[i][j] += delta_square * f[i][j];
-          u[i][j] *= 0.25;
+          u[i][j] = 0.25* (u_old[i - 1][j] + u_old[i + 1][j] + u_old[i][j - 1] + u_old[i][j + 1] + delta_square * f[i][j]);
           dist = dist + (u[i][j] - u_old[i][j]) * (u[i][j] - u_old[i][j]);
         }
       }
-      // printf("---> DONE: Line %d\n", __LINE__);
+
+      k += 1;
       #pragma omp single
       {
         c_dist = dist;
